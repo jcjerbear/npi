718cf36123922d7b4cff4c1f87c386cbfecdb0f4
zhuwangz <2010zhuYU!>
2018-12-12 06:02:21 -0800

    :sparkles: add testing

--- main.py
+++ main.py
@@ -5,13 +5,13 @@ import tensorflow as tf
 
 from tasks.addition.env.generate_data import generate_addition
 from tasks.addition.eval import evaluate_addition
-from tasks.addition.train import train_addition
+from tasks.addition.train import train_addition, test_addition
 
 
 FLAGS = tf.app.flags.FLAGS
 tf.app.flags.DEFINE_string("task", "addition", "Which NPI Task to run - [addition].")
 tf.app.flags.DEFINE_boolean("generate", False, "Boolean whether to generate training/test data.")
-tf.app.flags.DEFINE_integer("num_training", 1000, "Number of training examples to generate.")
+tf.app.flags.DEFINE_integer("num_training", 900, "Number of training examples to generate.")
 tf.app.flags.DEFINE_integer("num_test", 100, "Number of test examples to generate.")
 
 tf.app.flags.DEFINE_boolean("do_train", True, "Boolean whether to continue training model.")
@@ -29,7 +29,8 @@ def main(_):
         # Train Model (if necessary)
         if FLAGS.do_train:
             train_addition(FLAGS.num_epochs)
-
+            test_addition()
+          
         # Evaluate Model
         if FLAGS.do_eval:
             evaluate_addition()
